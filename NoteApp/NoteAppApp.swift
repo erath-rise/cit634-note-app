83c98e3c3d6725c33fa93a5ccce47dc62411f4ce
Lianne <ideng0212@gmail.com>
2025-10-20 11:16:44 +0100

    Initial Commit

---
+++ NoteApp/NoteAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoteAppApp.swift
+//  NoteApp
+//
+//  Created by 阿邓 on 20/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct NoteAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
