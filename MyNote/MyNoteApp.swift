07b0879b17b66398f40335f21e89a1bd934e2663
Lianne <ideng0212@gmail.com>
2025-11-14 11:36:33 +0000

    Initial Commit

---
+++ MyNote/MyNoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyNoteApp.swift
+//  MyNote
+//
+//  Created by 阿邓 on 14/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct MyNoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
